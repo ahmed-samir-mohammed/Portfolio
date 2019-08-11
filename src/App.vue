5d91bcbe0ee917ddefde38a888c6bc8791dc42e8
Ahmed Samir <samierahmed37@gmail.com>
2019-08-11 07:52:34 +0200

    One Update - UI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template lang="pug">
-
   #app
     // Navbar
     .nav
@@ -14,54 +13,30 @@
         router-link(to="/contact") Contact 
     // Social
     .social
-      
+      ul.social-list
+        li.social-item 
+          a(href="https://www.facebook.com/ahmed.azam.102")
+            img(src="./assets/SVG/facebook.svg", alt="alt")
+        li.social-item 
+          a(href="https://www.behance.net/ahmed-samir-mohammed")
+            img(src="./assets/SVG/behance.svg", alt="alt")
+        li.social-item 
+          a(href="https://www.instagram.com/a7med_samier/")
+            img(src="./assets/SVG/instagram.svg", alt="alt")
+        li.social-item 
+          a(href="https://www.linkedin.com/in/ahmed-samir-61150a155/")
+            img(src="./assets/SVG/linkedin.svg", alt="alt")
+
     .container 
+    <transition name="slide-fade">
       router-view
+    </transition>
+      
 </template>
+
 
 <style lang="scss">
 
 @import './sass/main';
 
-#app {
-  font-family: 'Cairo', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #fff;
-  background-image: url('./assets/Home.png');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding: 0;
-  margin: 0;
-  height: 100vh;
-}
-
-.nav {
-  padding: .5rem 1rem;
-  font-weight: normal;
-  background-color: rgba($color: #000000, $alpha: .5);
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  .nav-left {
-    .logo {
-      img {
-        width: 4rem
-      }
-    }
-  }
-  .nav-right {
-    a {
-      text-decoration: none;
-      margin: .5rem;
-      color: #fff;
-      &.router-link-exact-active {
-        border-bottom: 1px solid #ccc
-      }
-    }
-
-  }
-}
 </style>
